9694e1ec1828c43d4bac053f167eaa9f074b10f5
picev0 <ecotech3690s@yahoo.co.jp>
2020-10-18 17:04:42 +0900

    Initial Commit

---
+++ LabelSample/LabelSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabelSampleApp.swift
+//  LabelSample
+//
+//  Created by Shuhei Murata on 2020/10/18.
+//
+
+import SwiftUI
+
+@main
+struct LabelSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
