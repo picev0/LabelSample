0f6bf97d3c4c305261ddf5ae5610ac6c221868d4
picev0 <ecotech3690s@yahoo.co.jp>
2020-10-18 21:02:37 +0900

    Update ContentView.swift

--- LabelSample/ContentView.swift
+++ LabelSample/ContentView.swift
@@ -9,8 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 8) {
+            Label("Rain", systemImage: "cloud.rain")
+            Label("Snow", systemImage: "snow")
+            Label("Sun", systemImage:
+                  "sun.max")
+            Label("SwiftUI", systemImage: "swiftui").font(.system(size:48))
+            Label {
+                Text("SwiftUI Label")
+                    .foregroundColor(.primary)
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Color.gray.opacity(0.2))
+                    .clipShape(Capsule())
+            } icon: {
+                Rectangle()
+                    .fill(Color.blue)
+                    .frame(width: 64, height: 64)
+            }
+        }
     }
 }
 
